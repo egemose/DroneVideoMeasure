8ff4db8240948eb839c74a03668269f283632abb
Henrik Dyrberg Egemose <hes1990@gmail.com>
2019-11-21 12:47:21 +0100

    Only keep secrets in the app_config

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
+from flask import Flask, send_from_directory
+from flask_dropzone import Dropzone
+from flask_obscure import Obscure
 import argparse
-from app_config import app
 from projects import projects_view
 from videos import videos_view
 from misc import misc_view
 from drones import drones_view
+from app_config import AppConfig
+
+
+def serve_projects_file(filename):
+    return send_from_directory('projects', filename)
+
+# AppConfig object holds:
+# SECRET_KEY = 'some secret'
+# WTF_CSRF_SECRET_KEY = 'csrf secret key'
+# OBSCURE_SALT = some random int
+
+# Dropzone settings
+AppConfig.DROPZONE_UPLOAD_MULTIPLE = True
+AppConfig.DROPZONE_MAX_FILE_SIZE = 100000
+AppConfig.DROPZONE_PARALLEL_UPLOADS = 20
+AppConfig.DROPZONE_TIMEOUT = 1800000
+
+app = Flask(__name__)
+app.config.from_object(AppConfig)
+app.add_url_rule('/projects/<path:filename>', endpoint='projects_folder', view_func=serve_projects_file)
+dropzone = Dropzone(app)
+obscure = Obscure(app)
 
 app.register_blueprint(projects_view)
 app.register_blueprint(videos_view)
@@ -13,12 +37,12 @@ app.register_blueprint(drones_view)
 
 def parse_args():
     with open('version.txt') as version_file:
-            version = version_file.read()
-    parser = argparse.ArgumentParser(description='Image Annotater Flask App.', prog='Image Annotater')
+        version = version_file.read()
+    parser = argparse.ArgumentParser(description='Drone Footage Measure', prog='DFM')
     parser.add_argument('--debug', action='store_true', help='Enable debug mode.')
     parser.add_argument('--version', action='version', version='%(prog)s ' + version)
-    args = parser.parse_args()
-    return args
+    arguments = parser.parse_args()
+    return arguments
 
 
 if __name__ == '__main__':
--- app_config.py
+++ app_config.py
@@ -1,28 +1,5 @@
-from flask import Flask, send_from_directory
-from flask_dropzone import Dropzone
-from flask_obscure import Obscure
-
-
 class AppConfig:
-        # Flask settings
-    SECRET_KEY = 'This is an INSECURE secret!! DO NOT use this in production!!'
-    WTF_CSRF_SECRET_KEY = 'a csrf secret key34643643'
-
-    # Dropzone settings
-    DROPZONE_UPLOAD_MULTIPLE = True
-    DROPZONE_MAX_FILE_SIZE = 100000
-    DROPZONE_PARALLEL_UPLOADS = 20
-    DROPZONE_TIMEOUT = 1800000
-    # Obscure settings
-    OBSCURE_SALT = 2076864838
-
-
-def serve_projects_file(filename):
-    return send_from_directory('projects', filename)
-
-
-app = Flask(__name__)
-app.config.from_object(__name__+'.AppConfig')
-app.add_url_rule('/projects/<path:filename>', endpoint='projects_folder', view_func=serve_projects_file)
-dropzone = Dropzone(app)
-obscure = Obscure(app)
+    # Change these values before use.
+    SECRET_KEY = 'This is an INSECURE secret!! DO NOT use this!!'
+    WTF_CSRF_SECRET_KEY = 'a csrf secret key. Please change!'
+    OBSCURE_SALT = 2076864838  # just some random integer number
