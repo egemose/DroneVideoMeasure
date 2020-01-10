158b86eb5d3685e25b72a86b318579c11bb58f64
Henrik Dyrberg Egemose <hesc@mmmi.sdu.dk>
2020-01-10 12:22:09 +0100

    added shell script for start and stop.

---
+++ dvm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+platform="Linux" # Assumed
+uname=$(uname)
+case $uname in
+	"Darwin")
+	platform="MacOS / OSX"
+	;;
+	MINGW*)
+	platform="Windows"
+	;;
+esac
+
+if [[ $platform = "Windows" ]]; then
+	command="docker-compose -f docker-compose.windows.yml"
+else
+	command="docker-compose -f docker-compose.yml"
+fi
+
+if [[ $1 = "start" ]]; then
+	command="$command up"
+elif [[ $1 = "stop" ]]; then
+	command="$command down"
+elif [[ $1 = "rebuild" ]]; then
+	command="$command build"
+fi
+
+eval "$command"
