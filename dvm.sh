68b7b9016e2bfbb25d857cf0377f55e85a478516
Henrik Dyrberg Egemose <hesc@mmmi.sdu.dk>
2020-01-28 16:24:29 +0100

    check if user is in docker group

--- dvm.sh
+++ dvm.sh
@@ -10,10 +10,21 @@ case $uname in
 	;;
 esac
 
+command=""
+if [[ $platform = "Linux" ]]; then
+	eval "mkdir -p data"
+	if groups $USER | grep &>/dev/null '\bdocker\b'; then
+		echo "User in docker group. no need for sudo"
+	else
+		echo "User not in docker group. Using sudo!"
+	    	command="$command sudo"
+	fi
+fi
+
 if [[ $platform = "Windows" ]]; then
-	command="docker-compose -f docker-compose.windows.yml"
+	command="$command docker-compose -f docker-compose.windows.yml"
 else
-	command="mkdir -p data && docker-compose -f docker-compose.yml"
+	command="$command docker-compose -f docker-compose.yml"
 fi
 
 if [[ $1 = "start" ]]; then
