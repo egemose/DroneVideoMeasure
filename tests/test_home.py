a24a47879beeb228df3e776c3bb6d18b5cb30fef
Henrik Dyrberg Egemose <hde@mmmi.sdu.dk>
2025-06-10 01:35:11 +0200

    added more tests

--- tests/test_home.py
+++ tests/test_home.py
@@ -13,3 +13,15 @@ def test_home_page(client: FlaskClient, init_database: SQLAlchemy) -> None:
     response = client.get("/")
     assert response.status_code == 200
     assert b"Drone Video Measure" in response.data
+    assert b"Usage" in response.data
+    assert b"Supported Drones" in response.data
+    assert b"Author" in response.data
+    assert b"License" in response.data
+    assert b"Henrik Dyrberg Egemose" in response.data
+    assert b"SDU UAS Center" in response.data
+
+
+def test_version_page(client: FlaskClient, init_database: SQLAlchemy) -> None:
+    response = client.get("/version")
+    assert response.status_code == 200
+    assert b"You are using version" in response.data
