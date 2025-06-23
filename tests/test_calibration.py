e67ebdfa5106db3450ed2635d385fdab87c727a7
Henrik Dyrberg Egemose <hde@mmmi.sdu.dk>
2025-06-23 23:20:31 +0200

    added test for calibration

---
+++ tests/test_calibration.py
@@ -0,0 +1,42 @@
+from __future__ import annotations  # noqa: I001
+
+from pathlib import Path
+
+import pytest
+
+import numpy as np
+
+from flask.testing import FlaskClient
+
+from dvm.calibration.calibration import CalibrateCamera
+from dvm.calibration.corner_detector import ChessBoardCornerDetector
+
+
+def test_init_calibrate_camera() -> None:
+    cc = CalibrateCamera()
+    assert isinstance(cc.detector, ChessBoardCornerDetector)
+
+
+def test_calibrate_from_image(client: FlaskClient) -> None:
+    cc = CalibrateCamera()
+    image_files = [Path("./tests/test_data/calibration.jpg").resolve()]
+    mtx, dist, image_size, n_images = cc.calibrate_camera_from_images(image_files)
+    mtx_test = np.array([[1.83427403e03, 0, 9.87014485e02], [0, 1.83308269e03, 5.78589467e02], [0, 0, 1]])
+    dist_test = np.array([[6.489003e-02, -6.948572e-01, -3.838770e-04, 8.868022e-04, 2.579142]])
+    np.testing.assert_allclose(mtx, mtx_test)
+    np.testing.assert_allclose(dist, dist_test, rtol=1e-6)
+    assert image_size == (2048, 1080)
+    assert n_images == 1
+
+
+def test_calibrate_from_call(client: FlaskClient) -> None:
+    cc = CalibrateCamera()
+    image_files = Path("./tests/test_data/").resolve()
+    mtx, dist, fov_x, fov_y, n_images = cc(in_folder=image_files)
+    mtx_test = np.array([[1.83427403e03, 0, 9.87014485e02], [0, 1.83308269e03, 5.78589467e02], [0, 0, 1]])
+    dist_test = np.array([[6.489003e-02, -6.948572e-01, -3.838770e-04, 8.868022e-04, 2.579142]])
+    np.testing.assert_allclose(mtx, mtx_test)
+    np.testing.assert_allclose(dist, dist_test, rtol=1e-6)
+    assert pytest.approx(fov_x) == 58.330549
+    assert pytest.approx(fov_y) == 32.815780
+    assert n_images == 1
